fedd93afdf2a5a84921f7616354e30137e9be053
svkk <sairaviqar@gmail.com>
2017-05-29 16:53:43 +1000

    Working on creating static files with analysed data

--- public/js/Dashboard.js
+++ public/js/Dashboard.js
@@ -104,7 +104,7 @@ function tagDataService($http, $q) {
 
     function getStaticFile(callback) {
 
-        $http.get('/dh/staticFileTest.jsonz').success(getStaticSuccess,getStaticError);
+        $http.get('/dh/weightsData.jsonz').success(getStaticSuccess,getStaticError);
 
         function getStaticSuccess(results){
 

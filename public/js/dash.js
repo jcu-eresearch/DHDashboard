e22ad319d271f3497cea26d22e3ee56e4aabf921
svkk <sairaviqar@gmail.com>
2017-05-31 13:57:39 +1000

    Working on creating static files with analysed data

--- public/js/dash.js
+++ public/js/dash.js
@@ -9,28 +9,109 @@ homesteadApp.controller('dashController', function($scope, $timeout, tagDataServ
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: -19.66574, lng: 146.8462},
             mapTypeId: 'hybrid',
-            zoom:12
+            zoom:14
         });
+
+        map.data.setStyle({
+            strokeColor: '#66bb6a',
+            fillColor: '#66bb6a',
+            fillOpacity: 0.1,
+            strokeWeight: 2
+        });
+
+        map.data.loadGeoJson(
+            'data/paddocks.json');
 
         var marker1 = new google.maps.Marker({
             position: {lat: -19.66882, lng: 146.864},
             map: map,
             title: "Spring Creek"
         });
+
+        var marker11 = new google.maps.Marker({
+            position: {lat: -19.66882, lng: 146.864},
+            map: map,
+            label: {
+                text: "Spring Creek",
+                color: "white"
+            },
+            title: "Spring Creek",
+            icon: {
+                labelOrigin: new google.maps.Point(11, 50),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
+        });
+
         var marker2 = new google.maps.Marker({
             position: {lat: -19.657496, lng: 146.835306},
             map: map,
             title: "Digital Homestead"
         });
+
+        var marker22 = new google.maps.Marker({
+            position: {lat: -19.657496, lng: 146.835306},
+            map: map,
+            label: {
+                text:  "Digital Homestead",
+                color: "white"
+            },
+            title: "Digital Homestead",
+            icon: {
+                labelOrigin: new google.maps.Point(11, 50),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
+        });
+
         var marker3 = new google.maps.Marker({
             position: {lat: -19.66574, lng: 146.8462},
             map: map,
             title: "Double Barrel"
         });
+
+        var marker33 = new google.maps.Marker({
+            position: {lat: -19.66574, lng: 146.8462},
+            map: map,
+            label: {
+                text:  "Double Barrel",
+                color: "white"
+            },
+            title: "Double Barrel",
+            icon: {
+                labelOrigin: new google.maps.Point(11, 50),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
+        });
+
         var marker4 = new google.maps.Marker({
             position: {lat: -19.66872, lng: 146.8642},
             map: map,
             text: "Junction"
+        });
+
+        var marker44 = new google.maps.Marker({
+            position: {lat: -19.66872, lng: 146.8642},
+            map: map,
+            label: {
+                text:  "Junction",
+                color: "white"
+            },
+            title: "Junction",
+            icon: {
+                labelOrigin: new google.maps.Point(50, 20),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
         });
     };
 
--- public/js/map.js
+++ public/js/map.js
@@ -67,7 +67,7 @@ homesteadApp.controller('mapController', function($scope, tagDataService, $rootS
     $scope.map;
     $scope.heatmapOverlay;
 
-    $scope.soil=true;
+    $scope.soil=false;
     $scope.topology=false;
     $scope.paddocks=false;
     $scope.heatmap=true;
@@ -156,6 +156,7 @@ homesteadApp.controller('mapController', function($scope, tagDataService, $rootS
         var bounds = new google.maps.LatLngBounds(swBound, neBound);
         var srcImage = 'data/paddSoil.png';
         $scope.soilOverlay = new imgOverlay(bounds, srcImage, map);
+        $scope.soilOverlay.removeFromMap();
 
         var neBound1 = new google.maps.LatLng(-19.6522016454622, 146.87255651879877);
         var swBound1 = new google.maps.LatLng( -19.6767606432209, 146.83365885162357);
@@ -164,25 +165,108 @@ homesteadApp.controller('mapController', function($scope, tagDataService, $rootS
         $scope.topologyOverlay = new imgOverlay(bounds1, srcImage1, map);
         $scope.topologyOverlay.removeFromMap();
 
+        map.data.setStyle({
+            strokeColor: '#66bb6a',
+            fillColor: '#66bb6a',
+            fillOpacity: 0.1,
+            strokeWeight: 2
+        });
+
+        map.data.loadGeoJson(
+            'data/paddocks.json');
+
+
+
         var marker1 = new google.maps.Marker({
             position: {lat: -19.66882, lng: 146.864},
             map: map,
             title: "Spring Creek"
         });
+
+        var marker11 = new google.maps.Marker({
+            position: {lat: -19.66882, lng: 146.864},
+            map: map,
+            label: {
+                text: "Spring Creek",
+                color: "white"
+            },
+            title: "Spring Creek",
+            icon: {
+                labelOrigin: new google.maps.Point(11, 50),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
+        });
+
         var marker2 = new google.maps.Marker({
             position: {lat: -19.657496, lng: 146.835306},
             map: map,
             title: "Digital Homestead"
         });
+
+        var marker22 = new google.maps.Marker({
+            position: {lat: -19.657496, lng: 146.835306},
+            map: map,
+            label: {
+                text:  "Digital Homestead",
+                color: "white"
+            },
+            title: "Digital Homestead",
+            icon: {
+                labelOrigin: new google.maps.Point(11, 50),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
+        });
+
         var marker3 = new google.maps.Marker({
             position: {lat: -19.66574, lng: 146.8462},
             map: map,
             title: "Double Barrel"
         });
+
+        var marker33 = new google.maps.Marker({
+            position: {lat: -19.66574, lng: 146.8462},
+            map: map,
+            label: {
+                text:  "Double Barrel",
+                color: "white"
+            },
+            title: "Double Barrel",
+            icon: {
+                labelOrigin: new google.maps.Point(11, 50),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
+        });
+
         var marker4 = new google.maps.Marker({
             position: {lat: -19.66872, lng: 146.8642},
             map: map,
             text: "Junction"
+        });
+
+        var marker44 = new google.maps.Marker({
+            position: {lat: -19.66872, lng: 146.8642},
+            map: map,
+            label: {
+                text:  "Junction",
+                color: "white"
+            },
+            title: "Junction",
+            icon: {
+                labelOrigin: new google.maps.Point(50, 20),
+                url: 'default_marker.png',
+                size: new google.maps.Size(22, 40),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(11, 40),
+            }
         });
 
         tagDataService.getLocationData(renderMovementData);
